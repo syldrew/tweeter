aa91e16347dd35f97a0868c52102ab9f4578a0f7
syldrew <syl_peke14@hotmail.com>
2023-07-10 11:00:03 -0500

    Fetching tweets with Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,32 +1,50 @@
-// Fake data taken from initial-tweets.json
-const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
+// // Fake data taken from initial-tweets.json
+// const data = [
+//     {
+//       "user": {
+//         "name": "Newton",
+//         "avatars": "https://i.imgur.com/73hZDYK.png"
+//         ,
+//         "handle": "@SirIsaac"
+//       },
+//       "content": {
+//         "text": "If I have seen further it is by standing on the shoulders of giants"
+//       },
+//       "created_at": 1461116232227
+//     },
+//     {
+//       "user": {
+//         "name": "Descartes",
+//         "avatars": "https://i.imgur.com/nlhLi3I.png",
+//         "handle": "@rd"
+//       },
+//       "content": {
+//         "text": "Je pense , donc je suis"
+//       },
+//       "created_at": 1461113959088
+//     }
+//   ];
 
   $(document).ready(function() {
       const $form = $("form");
+
+
+//The loadtweets function will use jQuery to make a request to /tweets and receive the array of tweets as JSON.
+      const loadTweets = function() {
+        // Make a GET request to the server using Ajax
+        $.ajax({
+          url: "http://localhost:8080/tweets",
+          method: "GET",
+        }).then((res) => {
+          console.log("render Tweets: ", res);
+          // Call the renderTweets function and pass the response as an argument
+          renderTweets(res);
+        });
+      };
+    
+      // Call the loadTweets function to initiate the GET request
+      loadTweets();
+
        // add an event listener that listens for the submit event
       $form.submit((event) => {
       // prevent the default behaviour of the submit event (data submission and page refresh)
@@ -39,7 +57,10 @@ const data = [
         url: "http://localhost:8080/tweets",
         method: "POST",
         data: formData,
+        }).then(() => {
+        loadTweets();
       });
+
       console.log(event);
       $form[0].reset();
     });
@@ -66,7 +87,7 @@ const createTweetElement = function(tweet) {
   
     const $content = $('<div>').addClass('content').text(tweet.content.text);
     const $footer = $('<footer>');
-    const $timestamp = $('<span>').addClass('timestamp').text(tweet.created_at);
+    const $timestamp = $('<span>').addClass('timestamp').text(tweet.created_at).text(timeago.format(tweet.created_at, "en_US"));
     const $icons = $('<span>').addClass('icons').html('<i class="fas fa-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i>');
     $footer.append($timestamp, $icons);
     $tweet.append($header, $content, $footer);
@@ -74,4 +95,4 @@ const createTweetElement = function(tweet) {
     return $tweet;
   };
   
-  renderTweets(data);
+//   renderTweets(data); //Remove the hard-coded tweets object that we added earlier in order to drive our renderTweets function.
