7ba3c73dffac49e3289afacc1c8b77ec9e41c5a7
syldrew <syl_peke14@hotmail.com>
2023-07-12 09:24:42 -0500

    Removed blank spaces, and add relevant comments

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,42 +1,35 @@
  $(document).ready(function() {
-      const $form = $("form");
-         //The loadtweets function will use jQuery to make a request to /tweets and receive the array of tweets as JSON.
+      const $form = $("form");   
       const $error = $("#error");
-      const loadTweets = function() {
-        // Make a GET request to the server using Ajax
-        $.ajax({
+      
+      const loadTweets = function() {                                        //The loadtweets function will use jQuery to make a request to /tweets and receive the array of tweets as JSON.
+        $.ajax({ 
           url: "http://localhost:8080/tweets",
           method: "GET",
         }).then((res) => {
           console.log("render Tweets: ", res);
-        // Call the renderTweets function and pass the response as an argument
           renderTweets(res);
         });
       };
-        // Call the loadTweets function to initiate the GET request
+   
       loadTweets();
-        // Add an event listener that listens for the submit event
-      $form.submit((event) => {
-        // Prevent the default behaviour of the submit event (data submission and page refresh)
-      event.preventDefault(); //use event.preventDefault() to prevent the default form submission behaviour.
-      $error.hide();
-        // The jQuery .serialize() function turns a set of form data into a query string. This serialized data 
-        // Should be sent to the server in the data field of the AJAX POST request.
-      const formData = $form.serialize(); 
-        //Form Validation. Use the built-in browser alert function
-      const tweetContent = $form.find("textarea[name='text']").val().trim();
+        
+      $form.submit((event) => {                                                // Add an event listener that listens for the submit event
+      event.preventDefault();                                                  //use event.preventDefault() to prevent the default form submission behaviour.
+      $error.hide();                                                           //Hide error element.
+        
+      const formData = $form.serialize();                                      // The jQuery .serialize() function turns a set of form data into a query string. This serialized data 
+      const tweetContent = $form.find("textarea[name='text']").val().trim();   //Form Validation. Use the built-in browser alert function
       if (tweetContent === "") {
-        // alert("Please write a Tweet!");
         showError("Please write a Tweet!")
         return;
       }
       if (tweetContent.length > 140) {
-        // alert("This tweet is to long!!!!");
-        showError("This tweet is to long!!!!")
+        showError("This tweet is too long!!!!")
         return;
       }
-       // create an AJAX POST request in client.js that sends the form data to the server.
-      $.ajax({
+       
+      $.ajax({ 
         url: "http://localhost:8080/tweets",
         method: "POST",
         data: formData,
@@ -49,35 +42,35 @@
       $(".invalid").slideUp();
     });
 
-   // Function to show the error message
-   const showError = function(message) {
+   const showError = function(message) {                 // Function to show the error message
     $error.find("#message").text(message);
-    $error.slideDown(); //Use the slideDown jQuery function for some simple animation.
+    $error.slideDown();                                  //Use the slideDown jQuery function for some simple animation.
    };
   });
+
   const renderTweets = function(tweets) {
-     // Clear previous tweets
-    $('#tweets-container').empty();
-     // loops through tweets
-    for (const tweet of tweets) {
-     // Calls createTweetElement for each tweet
-      const $tweetElement = createTweetElement(tweet);
+    $('#tweets-container').empty();                       // Clear previous tweets
+    for (const tweet of tweets) {                         // loops through tweets
+     const $tweetElement = createTweetElement(tweet);     // Calls createTweetElement for each tweet
          $('#tweets-container').prepend($tweetElement);
     }
   };
-     /* Your code for creating the tweet element */
+
+     // Your code for creating the tweet element 
     const createTweetElement = function(tweet) {
     const $tweet = $('<article>').addClass('tweet');
-    const $header = $('<header>');
+    const $header = $("<div>").addClass("article-header");
+    const $avatarContainer = $("<div>");                   
     const $avatar = $('<img>').addClass('avatar').attr('src', tweet.user.avatars);
     const $name = $('<span>').addClass('name').text(tweet.user.name);
     const $handle = $('<span>').addClass('handle').text(tweet.user.handle);
     $header.append($avatar, $name, $handle);
-  
     const $content = $('<div>').addClass('content').text(tweet.content.text);
     const $footer = $('<footer>');
     const $timestamp = $('<span>').addClass('timestamp').text(tweet.created_at).text(timeago.format(tweet.created_at, "en_US"));
-    const $icons = $('<span>').addClass('icons').html('<i class="fas fa-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i>');
+    const $icons = $('<span>').addClass('icons').html('<i class="fas fa-flag"></i> <i class="fas fa-retweet"></i> <i class="fas fa-heart"></i>');
+    $avatarContainer.append($avatar, $name);
+    $header.append($avatarContainer, $handle);
     $footer.append($timestamp, $icons);
     $tweet.append($header, $content, $footer);
   
